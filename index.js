43ca53bb1094340a3d214e16ca034c13b4795c23
Arun Singh <48112517+arunsingh28@users.noreply.github.com>
2021-05-28 11:31:08 +0530

    server

--- index.js
+++ index.js
@@ -11,5 +11,6 @@ app.use('/gql', graphqlHTTP({
     graphiql: true
 }))
 
+const PORT = process.env.PORT || 3000
 
-app.listen(3000, (console.log('server running...')))
+app.listen(PORT, (console.log(`server running on PORT${PORT}`)))

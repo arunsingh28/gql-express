b4e97a4a83c458c7166b31c31ab690ddf0fe327f
Arun Singh <48112517+arunsingh28@users.noreply.github.com>
2021-05-28 11:30:01 +0530

    resolver notation

--- resolver.js
+++ resolver.js
@@ -2,12 +2,14 @@ const { user } = require('./data')
 
 exports.root = {
     getPerson: ({id}) => {
+        //  return data after filtering according to id       
         return user.find(n => n.id === id)
     },
     getAllPerson:()=>{
        return user
     },
     updatePersonName:(name)=>{
+        // show data from query        
         console.log(name)
         return name
     }
